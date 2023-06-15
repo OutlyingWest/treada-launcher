590f65f8dad11cdf81d108e2529321eb630d8338
OutlyingWest <alexeybuv7@gmail.com>
2023-06-15 19:09:21 +0300

    to stages was included to manager

--- TreadaTx_C/io_handler.py
+++ TreadaTx_C/io_handler.py
@@ -5,17 +5,18 @@ import time
 import keyboard
 
 
-async def main():
+def main():
     path_to_executable = sys.argv[1]
     exec_process = exe_runner(exe_path=path_to_executable)
     capturer = StdoutCapturer(process=exec_process)
     output_path = path_to_executable.strip('. ').split('.')[0]
 
     # Tasks for asynchronous execution
-    tasks = [
-        capturer.stream_manage(path_to_txt=output_path),
-    ]
-    await asyncio.gather(*tasks)
+    # tasks = [
+    #     capturer.stream_manage(path_to_txt=output_path),
+    # ]
+    # await asyncio.gather(*tasks)
+    capturer.stream_manage(path_to_txt=output_path)
 
 
 def exe_runner(exe_path: str) -> subprocess.Popen:
@@ -35,50 +36,60 @@ class StdoutCapturer:
         # Shut down shortcut configure
         self.running_flag = True
 
-    async def stream_manage(self, path_to_txt: str):
+    def stream_manage(self, path_to_txt=None):
         """
         Divides data from *.exe stdout to its own stdout and file with name *_output.txt.
         Ends by KeyboardInterrupt or ending condition satisfaction
         """
-        str_counter = 0
-        num_of_str = int(sys.argv[2])
 
         # Strip slashes if only file name was used as a path (for solving of powershell issues)
-        if path_to_txt.count('/') <= 2 or path_to_txt.count('\\') <= 2:
-            path_to_txt = path_to_txt.strip('/\\')
+        if path_to_txt:
+            if path_to_txt.count('/') <= 2 or path_to_txt.count('\\') <= 2:
+                path_to_txt = path_to_txt.strip('/\\')
 
-        with open(f'{path_to_txt.split(".")[0]}_output.txt', "w") as output_file:
-            start_time = time.time()
-            while num_of_str > str_counter and self.running_flag:
-                try:
-                    # Get line from process object
-                    treada_output: bytes = self.process.stdout.readline()
-                    if treada_output == b'' and self.process.poll() is not None:
-                        break
-                    if treada_output:
-                        try:
-                            decoded_output = treada_output.decode('utf-8')
-                            # Write *.exe output to file
-                            output_file.write(decoded_output.rstrip('\n'))
-                        except UnicodeDecodeError:
-                            decoded_output = treada_output
-                        # Copy *.exe output to its own stdout
-                        print(decoded_output.strip())
-                        str_counter += 1
-                except KeyboardInterrupt:
-                    self.running_flag = False
+        if not path_to_txt:
+            self.__io_loop()
+        else:
+            with open(f'{path_to_txt.split(".")[0]}_raw_output.txt', "w") as output_file:
+                self.__io_loop(output_file)
 
-            end_time = time.time()
-            # Terminate main executable process
-            self.process.terminate()
-
-        execution_time = end_time - start_time
-        print('Number of strings:', str_counter)
-        print(f'Execution time:{execution_time:.2f}s')
+        # Terminate main executable process
+        self.process.terminate()
         # Errors' catching
         error = self.process.stderr.read().decode('utf-8')
         if error:
             print('ERRORS:', error.strip())
+
+    def __io_loop(self, output_file=None):
+
+        str_counter = 0
+        num_of_str = int(sys.argv[2])
+
+        start_time = time.time()
+        while num_of_str > str_counter and self.running_flag:
+            try:
+                # Get line from process object
+                treada_output: bytes = self.process.stdout.readline()
+                if treada_output == b'' and self.process.poll() is not None:
+                    break
+                if treada_output:
+                    try:
+                        decoded_output = treada_output.decode('utf-8')
+                        # Write *.exe output to file
+                        if output_file:
+                            output_file.write(decoded_output.rstrip('\n'))
+                    except UnicodeDecodeError:
+                        decoded_output = treada_output
+                    # Copy *.exe output to its own stdout
+                    print(decoded_output.strip())
+                    str_counter += 1
+            except KeyboardInterrupt:
+                self.running_flag = False
+
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print('Number of strings:', str_counter)
+        print(f'Execution time in I/O loop:{execution_time:.2f}s')
 
     async def keyboard_catch(self):
         pass
@@ -88,5 +99,6 @@ class StdoutCapturer:
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete((main()))
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete((main()))
+    main()
