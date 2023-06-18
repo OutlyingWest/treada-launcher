d4b1c3906603f1389875c229f8bc96537c1d4a96
OutlyingWest <alexeybuv7@gmail.com>
2023-06-18 21:08:37 +0300

    plotter titles added

--- wrapper/plotting.py
+++ wrapper/plotting.py
@@ -1,7 +1,12 @@
+import os
+import matplotlib
+
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
 import pandas as pd
 
-from config_builder import load_config
+from wrapper.config_builder import load_config
 
 
 def main():
@@ -12,11 +17,14 @@ def main():
     plot_builder(result_path)
 
 
-def plot_builder(res_path):
+def plot_builder(res_path: str):
     df = pd.read_csv(res_path, skiprows=11, header=0, sep='\s+')
+    u_value = res_path.rsplit(os.sep, maxsplit=1)[1].split('(')[1].rsplit(')', maxsplit=1)[0]
+    figure_title = 'Udrm = ' + u_value + ' V'
     plotter(df[df.columns[0]], df[df.columns[1]],
             x_label='time (ns)',
-            y_label='I (mA/cm^2)')
+            y_label='I (mA/cm^2)',
+            fig_title=figure_title)
 
     try:
         plt.show()
@@ -24,8 +32,13 @@ def plot_builder(res_path):
         pass
 
 
-def plotter(x, y, x_label='x', y_label='y'):
+def plotter(x, y, x_label='x', y_label='y', fig_title='Transient'):
     fig, ax = plt.subplots(1, 1)
+    # Set window title
+    window = fig.canvas.manager.window
+    window.title(fig_title)
+    # Set plot title
+    ax.set_title(fig_title)
     ax.grid(True)
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
@@ -33,4 +46,7 @@ def plotter(x, y, x_label='x', y_label='y'):
 
 
 if __name__ == '__main__':
+    from config_builder import load_config
     main()
+else:
+    from wrapper.config_builder import load_config
