edd1b1100a04e3be9fe098f06227d955d80e7253
OutlyingWest <alexeybuv7@gmail.com>
2023-06-30 15:12:01 +0300

    improved format of plot calls

--- wrapper/ui/plotting.py
+++ wrapper/ui/plotting.py
@@ -1,36 +1,47 @@
 import os
-import matplotlib
+import sys
+from typing import List, Tuple, Union
 
+import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-
 import pandas as pd
+
+from wrapper.config.config_builder import load_config
 
 
 def main():
-    config = load_config('wrapper/config.json')
-    print('Enter voltage from required file name to load data. Example: "u(-0.4)"')
-    path_ending = input()
-    result_path = config.paths.treada_result_output.rsplit('.', maxsplit=1)[0] + path_ending + '.txt'
-    plot_builder(result_path)
+    config = load_config('config.json')
+    result_path = os.path.split(config.paths.treada_result_output)[0] + os.sep
+    if len(sys.argv) > 1:
+        res_name = sys.argv[1]
+    else:
+        print('Enter file name to load data from data/result/. Example: "res_u(-0.45).txt"')
+        res_name = input()
+    full_result_path = result_path + res_name
+    plot_builder(full_result_path)
 
 
-def plot_builder(res_path: str):
-    df = pd.read_csv(res_path, skiprows=11, header=0, sep='\s+')
+def plot_builder(res_path: str, plot_path: Union[str, None] = None, skip_rows=11,
+                 special_points: List[Tuple[float, float]] = None):
+    df = pd.read_csv(res_path, skiprows=skip_rows, header=0, sep='\s+')
     u_value = res_path.rsplit(os.sep, maxsplit=1)[1].split('(')[1].rsplit(')', maxsplit=1)[0]
     figure_title = 'Udrm = ' + u_value + ' V'
     plotter(df[df.columns[0]], df[df.columns[1]],
-            x_label='time (ns)',
+            x_label='time (ps)',
             y_label='I (mA/cm^2)',
-            fig_title=figure_title)
+            fig_title=figure_title,
+            special_points=special_points)
 
+    if plot_path:
+        plt.savefig(plot_path)
     try:
         plt.show()
     except KeyboardInterrupt:
         pass
 
 
-def plotter(x, y, x_label='x', y_label='y', fig_title='Transient'):
+def plotter(x, y, x_label='x', y_label='y', fig_title='Transient', special_points: List[Tuple[float, float]] = None):
     fig, ax = plt.subplots(1, 1)
     # Set window title
     window = fig.canvas.manager.window
@@ -40,11 +51,18 @@ def plotter(x, y, x_label='x', y_label='y', fig_title='Transient'):
     ax.grid(True)
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
+    # Set and display special points on plot if they exist
+    if special_points:
+        for point in special_points:
+            special_x, special_y = point
+            plt.scatter(special_x, special_y, color='red', marker='o', s=30)  # Особая точка
+            plt.annotate(f'({special_x}, {special_y})', (special_x, special_y), xytext=(10, -20),
+                         textcoords='offset points', arrowprops=dict(arrowstyle='->', color='black'))
     ax.plot(x, y)
 
 
 if __name__ == '__main__':
-    from wrapper.config.config_builder import load_config
+    # Добавить путь к директории "wrapper" в переменную окружения PYTHONPATH
+    wrapper_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "wrapper"))
+    sys.path.append(wrapper_path)
     main()
-else:
-    from wrapper.config.config_builder import load_config
